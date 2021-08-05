1f24b7441963c14d72c3cadbfef2b8c17e4a24cf
Marco Santarelli <marco@backbase.com>
2021-08-05 14:14:25 +0200

    Updated examples to v3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,39 +1,45 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { ChartDataSets, ChartOptions } from 'chart.js';
-import { Color, Label } from 'ng2-charts';
+import { Component } from '@angular/core';
+import { ChartConfiguration, ChartOptions, ChartType } from "chart.js";
 
 @Component({
-  selector: 'my-app',
+  selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: [ './app.component.css' ]
 })
 export class AppComponent {
-  public lineChartData: ChartDataSets[] = [
-    { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' }
-  ];
-  public lineChartLabels: Label[] = [
-    'January',
-    'February',
-    'March',
-    'April',
-    'May',
-    'June',
-    'July'
-  ];
-  public lineChartOptions: ChartOptions & { annotation: any } = {
-    responsive: true
+  title = 'ng2-charts-demo';
+
+  public lineChartData: ChartConfiguration<'line'>['data'] = {
+    labels: [
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+      'July'
+    ],
+    datasets: [
+      {
+        data: [ 65, 59, 80, 81, 56, 55, 40 ],
+        label: 'Series A',
+        fill: true,
+        tension: 0.5,
+        borderColor: 'black',
+        backgroundColor: 'rgba(255,0,0,0.3)'
+      }
+    ]
   };
-  public lineChartColors: Color[] = [
-    {
-      borderColor: 'black',
-      backgroundColor: 'rgba(255,0,0,0.3)'
-    }
-  ];
+  public lineChartOptions: ChartOptions<'line'> = {
+    responsive: false
+  };
   public lineChartLegend = true;
-  public lineChartType = 'line';
   public lineChartPlugins = [];
 
-  constructor() {}
+  constructor() {
+  }
 
-  ngOnInit() {}
+  ngOnInit() {
+  }
+
 }
